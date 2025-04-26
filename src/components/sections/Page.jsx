08b775dd35d39935d292ae2d114f30b5e0bbc571
Divine Ugorji <divineugorji37@gmail.com>
2025-04-26 15:07:58 +0100

    Add shadow styling to contact section

--- src/components/sections/Page.jsx
+++ src/components/sections/Page.jsx
@@ -1,6 +1,8 @@
 function Page({ children }) {
   return (
-    <div className="bg-[var(--color-primary-90)] font-family">{children}</div>
+    <div className="bg-[var(--color-primary-90)] font-family pb-18">
+      {children}
+    </div>
   );
 }
 
